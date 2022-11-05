7519e82017f8552fce4b29bdedaf5cf8d87f8293
Your Name <you@example.com>
2022-11-05 21:21:20 +0100

    Dodavanje:LoginController,LogoutController,HomeController, RegisterController  home html,register html Promeni: loginservlet i logoutservlet vo mapping promena, loginfilter vo if dodavame register i login html promena

---
+++ src/main/java/mk/ukim/finki/auditoriskivezhbi/web/controller/LoginController.java
@@ -0,0 +1,46 @@
+package mk.ukim.finki.auditoriskivezhbi.web.controller;
+
+import mk.ukim.finki.auditoriskivezhbi.model.User;
+import mk.ukim.finki.auditoriskivezhbi.model.exceptions.InvalidUserCredentialsException;
+import mk.ukim.finki.auditoriskivezhbi.service.AuthService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    private final AuthService authService;
+
+    public LoginController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping
+    //@RequestMapping(method = RequestMethod.GET, value = "")
+    public String getLoginPage(){
+        return  "login";
+    }
+
+    @PostMapping
+    public String login(HttpServletRequest request, Model model){
+        User user=null;
+        try {
+            user=this.authService.login(request.getParameter("username"),request.getParameter("password"));
+            request.getSession().setAttribute("user",user);
+            return "redirect:/home";
+
+        }
+        catch (InvalidUserCredentialsException exception){
+            model.addAttribute("hasError",true);
+            model.addAttribute("error",exception.getMessage());
+            return "login";
+        }
+    }
+}
